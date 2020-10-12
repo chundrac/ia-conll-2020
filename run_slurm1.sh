2b4eb4e24a356f7a2518446b0ab593caef7d2e45
Chundra Cathcart <chundra.a.cathcart@gmail.com>
2020-10-12 16:42:39 +0200

    Update run_slurm1.sh

--- run_slurm1.sh
+++ run_slurm1.sh
@@ -1,7 +1,7 @@
 module use /sapps/etc/modules/start/
 module load generic
 
-for j in {0..8}; do
+for j in {0..7}; do
     sbatch run.sh run_model.py 0 $j lang
     sbatch run.sh run_model.py 0 $j langPOS
     sbatch run.sh run_model.py 0 $j langPOSsem
